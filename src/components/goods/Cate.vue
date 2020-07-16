72c795ea81e1301b20db5a7bb350b0f60f7f1bfe
luhix <296636619@qq.com>
2020-07-16 22:36:22 +0800

    完成商品分类开发

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -10,7 +10,7 @@
     <el-card>
       <el-row>
         <el-col>
-          <el-button type="primary">添加分类</el-button>
+          <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
         </el-col>
       </el-row>
 
@@ -62,6 +62,35 @@
         :total="total">
       </el-pagination>
     </el-card>
+
+    <!-- 添加分类对话框 -->
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed"
+      >
+      <el-form :model="addCateForm" :rules="addCateRules" ref="addCateFormRef" label-width="100px">
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类">
+          <el-cascader
+            v-model="selectedKeys"
+            :options="parentCateList"
+            :props="props"
+            @change="parentCateChange"
+            clearable
+          >
+          </el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 <script>
@@ -75,13 +104,30 @@ export default {
         pagesize: 5
       },
       cateList: [],
-      total: 0
-      // 为table指定列的定义
+      total: 0,
+      addCateDialogVisible: false,
+      addCateForm: {
+        cat_name: '',
+        cat_pid: 0,
+        cat_level: 0
+      },
+      addCateRules: {
+        cat_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' }
+        ]
+      },
+      parentCateList: [],
+      props: {
+        expandTrigger: 'hover',
+        checkStrictly: true
+      },
+      selectedKeys: [] // 选中的id
     }
   },
   created () {
     this.getGoodsCateList()
   },
+
   methods: {
     async getGoodsCateList () {
       const { data: res } = await this.$http.get('categories', { params: this.queryInfo })
@@ -100,6 +146,66 @@ export default {
     handleCurrentChange (newNum) {
       this.queryInfo.pagenum = newNum
       this.getGoodsCateList()
+    },
+    showAddCateDialog () {
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    // 获取父级分类的列表
+    async getParentCateList () {
+      const { data: res } = await this.$http.get('categories', { params: { type: 2 } })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取父级分类失败')
+      }
+      // 处理渲染
+      const news = res.data.map((item) => {
+        return {
+          value: item.cat_id,
+          label: item.cat_name,
+          children: item.children.map((item) => {
+            return {
+              value: item.cat_id,
+              label: item.cat_name
+            }
+          })
+        }
+      })
+      console.log(news)
+      this.parentCateList = news
+    },
+    parentCateChange (value) {
+      console.log(this.selectedKeys)
+      // 数组长度大于0， 选中父级分类
+      if (this.selectedKeys.length > 0) {
+        // 父级分类
+        this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+        this.addCateForm.cat_level = this.selectedKeys.length
+      } else {
+        this.addCateForm.cat_pid = 0
+        this.addCateForm.cat_level = 0
+      }
+    },
+    // 添加分类
+    addCate () {
+      this.$refs.addCateFormRef.validate(async (valid) => {
+        console.log(valid)
+        if (valid) {
+          const { data: res } = await this.$http.post('categories', this.addCateForm)
+          if (res.meta.status !== 201) {
+            return this.$message.error('获取商品分类失败')
+          }
+          this.$message.success('获取商品分类成功')
+          this.getGoodsCateList()
+          this.addCateDialogVisible = false
+        }
+      })
+    },
+    // 监听对话框的关闭事件，重置表单数据
+    addCateDialogClosed () {
+      this.$refs.addCateFormRef.resetFields()
+      this.selectedKeys = []
+      this.addCateForm.cat_level = 0
+      this.addCateForm.cat_pid = 0
     }
   }
 }
@@ -107,5 +213,8 @@ export default {
 <style lang="less" scoped>
 .el-table {
   margin-top: 15px;
+}
+.el-cascader-panel {
+  height: 200px !important;
 }
 </style>
